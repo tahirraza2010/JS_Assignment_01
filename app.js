6afb1cb40a6c1493935d900841bac207cbedae38
samiraza19 <samirazagb@gmail.com>
2024-09-08 03:30:53 -0700

    Work Complete

---
+++ app.js
@@ -0,0 +1,172 @@
+// Chapter1(alert)
+// Q1
+alert("Mohammad")
+alert("Tahir")
+alert("tahirraza@gmail.com")
+alert("03172218320 \n 123456")
+
+// Q2
+alert("You re Learing JavaScript!")
+
+// Q3
+alert("I am learing JavaScript")
+
+// Chapter 2 (Variables for string)
+// Q1
+var firstName ="Mohammad";
+alert(firstName)
+
+// Q2
+var age;
+age="15 years old"
+alert(age)
+
+// Q3
+var teamName="Development Team"
+alert(teamName)
+
+// Q4
+var bestMan="Tahir"
+alert(bestMan)
+
+// Chapter 3 (Variables for numbers)
+// Q1
+var caseQty = 15;
+alert(caseQty)
+
+// Q2
+ caseQty=144
+alert(caseQty)
+
+
+// Q3
+var num = "9";
+
+// Q4
+var Q4 
+Q4 = 15 + 7
+
+alert(Q4)
+
+// Q5
+var merchTotal = 160;
+var shippingCharge = 10;
+var orderTotal = merchTotal + shippingCharge;
+alert(orderTotal)
+
+
+// Q6
+var Q6n1 = 6
+Q6n1 += 8
+alert(Q6n1)
+
+
+
+
+// Chapter1(alert)
+// Q1
+alert("My name is Tahir")
+
+// Q2 
+alert("Error! Please enter a valid password")
+
+
+// Q3
+alert("Welcome to JS Land... \n Happy Coding!")
+
+// Q4
+alert("Welcome to Js Land...")
+alert("Happy Coding! \n Prevent this page from creating additonal dialogs.")
+
+// Q5
+alert("Hello... I can run Js through my web browse's console")
+
+// Chapter2 (VARIABLES FOR STRINGS)
+// Q1
+var username ="Tahir"
+alert(username)
+
+// Q2
+var myname ="Mohammad Tahir"
+alert(myname)
+
+// Q3
+var message 
+message=" Hello World "
+alert(message)
+
+// Q4
+var name ="Mohammad Tahir"
+var age ="15 years old"
+var field ="Certified Mobile Application Development "
+alert(name)
+alert(age)
+alert(field)
+
+// Q5
+var JS ="PIZZA \n PIZZ \n PIZ \n PI \n P"
+alert(JS)
+
+// Q6
+var email ="example@example.com"
+alert("My email address is " + email)
+
+// Q7
+var book="A smarter way to learn JavaScript."
+alert("I am trying to learn from the book" +book )
+
+// Q8
+document.write("Yah! I can write HTML contant through JavaScript ")
+
+// Q9
+alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
+document.write("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
+
+
+// Chapter3(VARIABLES FOR NUMBERS)
+// Q1
+var age ="15 years old"
+alert("I am "+age)
+
+// var num ="14"
+alert("You have visited this site " + num + " " + "time" )
+
+// Q2
+var birthyear="2010"
+document.write("My birth year is"+ " "+ birthyear +"<br> Data type of my ddecleard variable is number " )
+
+// Q3
+var message="“John Doe ordered 5 T-shirt(s) on XYZ Clothing store”."
+document.write(message)
+
+
+// Chapter4 (VARIABLE NAMES: LEGAL & ILLEGAL)
+// Q1
+var a = 10, b = 20, c = 30;
+alert(`The values are: a = ${a}, b = ${b}, c = ${c}`);
+
+// Q2
+ 
+// legal variable 
+var myname
+var myName
+var my_name
+var name123
+var name!
+
+// illegal variable
+// var 123name
+// var #name
+// var @name
+// var function
+// var my-name    
+
+// Q4
+document.write("Rules for naming JS variables")
+document.write("<br> Variable names can only contain number, $ and _.For example $my_1stVariable <br> Variables must begin with a letter, $ or_. For example $name, _name or name <br> Variable names are case sencitive <br> Variable names should not be JS keywords")
+
+
+
+
+
+
